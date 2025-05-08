31affc96a54e1a58954bb6a3e00a5fc8c66c2483
BrennoQ <brennoqueiros@gmail.com>
2025-05-07 21:47:12 -0300

    comentario

--- Projeto.py
+++ Projeto.py
@@ -1,7 +1,16 @@
 listaclientes = [{"nome":"Ana","idade":20},{"nome":"Maria","idade":30}]
 
 #while True
-
+ opcoes = input('1 - Cadastrar\n2 - Buscar\n3 - Deletar\n4 - Sair')
+    if opcoes == '4':
+        print('Saindo...')
+        break
+    if opcoes == '1':
+        cadastrar_clientes()
+    if opcoes == '2':
+        buscar_clientes()
+    if opcoes == '3':
+        deletar_clientes()
 
 
 
