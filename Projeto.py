63099d023127a864f49d1af1861e499a7f64b5da
rafaelgomes00j <9434rafaelgomes@gmail.com>
2025-05-07 21:59:16 -0300

    atualização 1.0

--- Projeto.py
+++ Projeto.py
@@ -1,10 +1,10 @@
 listaclientes = [{"nome":"Ana","idade":20},{"nome":"Maria","idade":30}]
 
-#while True
- opcoes = input('1 - Cadastrar\n2 - Buscar\n3 - Deletar\n4 - Sair')
+while True:
+    opcoes = input('1 - Cadastrar\n2 - Buscar\n3 - Deletar\n4 - Sair')
     if opcoes == '4':
         print('Saindo...')
-        break
+    break
     if opcoes == '1':
         cadastrar_clientes()
     if opcoes == '2':
