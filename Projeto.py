36c5f9ff37230c6979a6986bc5e5634a772ef445
BrennoQ <brennoqueiros@gmail.com>
2025-05-07 21:42:50 -0300

    comentario

---
+++ Projeto.py
@@ -0,0 +1,34 @@
+listaclientes = [{"nome":"Ana","idade":20},{"nome":"Maria","idade":30}]
+
+#while True
+
+
+
+
+
+
+
+#funcao cadastrar
+
+
+
+
+
+
+#funcao buscar
+
+
+
+
+
+
+#funcao deletar
+
+
+
+
+
+
+
+
+
